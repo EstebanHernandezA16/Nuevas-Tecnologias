af14c312c04ed95b10509d0ab22834265c1ebb16
SEBAS <js.sierramunerajuansebastian999@gmail.com>
2023-08-31 08:47:49 -0500

    Clase #4, hay reto en archivo, proxima semana examen

---
+++ Clase#4/diccionario.py
@@ -0,0 +1,13 @@
+contacto = {}
+
+contacto['nombre'] = 'Alejo'
+contacto['numero'] = '329103921'
+contacto['fecha_nac'] = '30/04/2001'
+contacto['direccion'] = 'cra 55#66b 31'
+contacto['correo'] = 'Aha@gmail.com'
+contacto['mascotas'] = True
+contacto['profesion'] = 'Programador'
+contacto['Titulo'] = 'Ingeniero'
+contacto['Color_fav'] = 'Rojo'
+contacto['Nacionalidad'] = 'Colombiano'
+
---
+++ Clase#4/ejercicio.py
@@ -0,0 +1,73 @@
+#.append
+#
+
+# lista = []
+
+contactos = {
+    'contacto1':{
+        'nombre':'Esteban',
+        'fecha_nac':'23/04/2012',
+        'direccion':'cra 55#66b31'
+    },
+    'contacto2':{
+        'nombre':'Alejo',
+        'fecha_nac':'30/09/1996',
+        'direccion':'cll 32#32-123'
+    },
+    'contacto3':{
+        'nombre':'Miguel',
+        'fecha_nac':'30/09/1996',
+        'direccion':'cll 32#32-123'
+    }
+}
+
+# contacto['nombre'] = 'Esteban'
+# contacto['fecha_nac'] = '24/02/2002'
+# contacto['direccion'] = 'cra 55#66b 52'
+
+# lista.append(contacto)
+
+# contacto['nombre'] = 'Esteban'
+# contacto['fecha_nac'] = '24/02/2002'
+# contacto['direccion'] = 'cra 55#66b 52'
+
+# lista.append(contacto)
+
+# contacto['nombre'] = 'Esteban'
+# contacto['fecha_nac'] = '24/02/2002'
+# contacto['direccion'] = 'cra 55#66b 52'
+
+# lista.append(contacto)
+
+# contacto['nombre'] = 'Esteban'
+# contacto['fecha_nac'] = '24/02/2002'
+# contacto['direccion'] = 'cra 55#66b 52'
+
+# lista.append(contacto)
+
+# contacto['nombre'] = 'Alejandro'
+# contacto['fecha_nac'] = '24/02/2002'
+# contacto['direccion'] = 'cra 55#66b 5233333'
+
+# contacto = contactos.get('contacto', 'contacto no encontrado')
+
+# print(f"contactos {contactos}")
+
+(contactos['contacto1'])['nombre'] = 'Alejandro'
+# mostrar valor especifico
+print(f" nombre de contacto1{(contactos.get('contacto1')).get('nombre')}")
+
+
+
+
+
+# print(f" Keys de contacto {contactos.keys()}")
+
+# print(f" Items de contacto {contactos.values()}")
+
+# print(f" Keys-Valores de contacto {contactos.items()}")
+# print(contacto)
+
+
+
+
